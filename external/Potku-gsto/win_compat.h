09e8c2dcfd8cf5eed7670fb9875b428ce22b80da
Atte Severi Jääskeläinen <severi.a.jaaskelainen@student.jyu.fi>
2018-02-12 12:10:32 +0200

    Poistin masterin sisällön.

--- external/Potku-gsto/win_compat.h
+++
@@ -1,5 +0,0 @@
-#ifdef WIN32
-#include <windows.h>
-#define sleep(x) Sleep(1000*x)
-char *strsep(char **, const char *);
-#endif
--- external/Potku-zbl96/local.h
+++
@@ -1,5 +0,0 @@
-
-#define F_ELEMENTS ../Potku-data/masses.dat
-#define F_SCOEFA   ../Potku-data/scoef.95a
-#define F_SCOEFB   ../Potku-data/scoef.95b
-
