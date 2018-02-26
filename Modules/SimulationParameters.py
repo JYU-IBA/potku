618443c8b4b9575842300cb29f51841e1d6a02d0
Atte Severi Jääskeläinen <severi.a.jaaskelainen@student.jyu.fi>
2018-02-26 15:25:19 +0200

    read_paramters function is now able to read parameters from a file. Still need to do some checking in case of errors and exceptions.

--- Modules/SimulationParameters.py
+++ Modules/SimulationParameters.py
@@ -2,3 +2,48 @@
 
 class SimulationParameters():
 
+    def read_parameters(filepath):
+        """ Read the simulation parameters from the MCERD input file """
+    
+        params = {
+            "Type of simulation:": None,
+            "Beam ion:": None,
+            "Beam energy:": None,
+            "Target description file:": None,
+            "Detector description file:": None,
+            "Recoiling atom:": None,
+            "Recoiling material distribution:": None,
+            "Target angle:": None,
+            "Beam spot size:": None,
+            "Minimum angle of scattering:": None,
+            "Minimum energy of ions:": None,
+            "Number of ions:": None,
+            "Number of ions in the presimulation:": None,
+            "Average number of recoils per primary ion:": None,
+            "Seed number of the random number generator:": None,
+            "Recoil angle width (wide or narrow):": None,
+            "Presimulation result file:": None,
+            "Minimum main scattering angle:": None,
+            "Beam divergence:": None,
+            "Beam profile:": None,
+            "Surface topography file:": None,
+            "Side length of the surface topography image:": None,
+            "Number of real ions per each scaling ion:": None
+        }
+        try:
+            with open(filepath) as file:
+                lines = file.readlines()
+            for line in lines:
+                line.lstrip()
+                for key, value in params.items():
+                    if line.startswith(key):
+                        val = line.partition(':')[2].lstrip().rstrip().split()
+                        if len(val) < 3:
+                            params[key] = val[0]
+                        else:
+                            params[key] = (val[0], val[1])
+    
+            for key, value in params.items():
+                print(value)
+        except IOError:
+            print('Could not read file ' + filepath)
