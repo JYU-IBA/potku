02f80e509c98217b938c5b37a5c4070c2c6dc944
Samuel Kaiponen <samuel.kaiponen@gmail.com>
2018-02-28 10:44:34 +0200

    Did some cleanup and added TODO for Windows and Mac versions

--- Modules/Simulation.py
+++ Modules/Simulation.py
@@ -1,42 +1,39 @@
 # coding=utf-8
-'''
-Created on 5.4.2013
-Updated on 20.8.2013
-'''
+"""
+Created on 26.2.2018
+Updated on 28.2.2018
+"""
 __author__ = ""
 __versio__ = "2.0"
 
-import os, platform, subprocess
+import os
+import platform
+import subprocess
 
-class Simulation():
+
+class Simulation:
 
     def __init__(self, command_file_path):
-        '''Inits Simulation.
+        """Inits Simulation.
 
         Args:
             command_file_path: Full path of where simulation command file is located.
-        '''
+        """
         self.bin_dir = '%s%s%s' % ('external', os.sep, 'Potku-bin')
-    #    self.command_win = 'cd ' + self.bin_dir + ' && tof_list.exe ' \
-    #                       + filepaths_str + ' | erd_depth.exe ' + outputpath + ' tof.in'
-        self.command_unix = 'cd ' + self.bin_dir + ' && ./mcerd ' \
-                             + '/home/siansiir/mcerd/source/Examples/35Cl-85-LiMnO_Li'
-#                            + command_file_path
-        print('init')
-
+        # TODO: Windows and Mac commands
+    #    self.command_win = 'cd ' + self.bin_dir + ' && mcerd.exe ' + command_file_path
+        self.command_unix = 'cd ' + self.bin_dir + ' && ./mcerd ' + command_file_path
 
     def run_simulation(self):
         used_os = platform.system()
-        if used_os == 'Windows':
-            subprocess.call(self.command_win, shell=True)
-        elif used_os == 'Linux':
+        if used_os == 'Linux':
             subprocess.call(self.command_unix, shell=True)
-    #    elif used_os == 'Darwin':
-    #        subprocess.call(self.command_unix, shell=True)
+        # TODO: Windows and Mac commands
+        # elif used_os == 'Windows':
+        #     subprocess.call(self.command_win, shell=True)
         else:
             print('It appears we do no support your OS.')
 
-        print('onnistui?')
 
-
-Simulation("").run_simulation()
+# For testing
+Simulation("/home/siansiir/mcerd/source/Examples/35Cl-85-LiMnO_Li").run_simulation()
