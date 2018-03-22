c93c239694ba9dd88be74d0e6b070503613d671f
Atte Severi Jääskeläinen <severi.a.jaaskelainen@student.jyu.fi>
2018-03-22 20:31:33 +0200

    Started refactoring the code.

--- Modules/Simulation.py
+++ Modules/Simulation.py
@@ -15,26 +15,23 @@ import os
 import platform
 import subprocess
 import logging
-import sys
 from Modules.Functions import md5_for_file
 from Modules.Settings import Settings
-import shutil
 from Modules.Null import Null
+from errno import EEXIST
 
 class Simulations:
     """Simulations class handles multiple simulations.
     """
 
-    def __init__(self, project):
+    def __init__(self, sample_directory):
         """Inits simulations class.
 
         Args:
             project: Project class object.
         """
-        self.project = project
-        self.simulations = {}  # Dictionary<Simulation>
-        # self.measuring_unit_settings = None
-        # self.default_settings = None
+        self._directory = os.path.join(sample_directory, "Simulations")
+        self.simulations = []
 
     def is_empty(self):
         """Check if there are any simulations.
@@ -45,96 +42,72 @@ class Simulations:
         """
         return len(self.simulations) == 0
 
-    def get_key_value(self, key):
-        if not key in self.simulations:
-            return None
-        return self.simulations[key]
+    def load_simulation(self, simulation_name):
+        """Loads a single simulation
+        """
 
-    def add_simulation_file(self, simulation_file, tab_id):
-        """Add a new file to simulations.
+        simulation_directory = os.path.join(self._directory, simulation_name)
+
+    def load_simulations(self):
+        """Loads all of the simulations in Simulations directory
+        """
+
+        simulations = os.listdir(self._directory)
+        # TODO: Remove non-directories from list
+        for simulation in simulations:
+
+
+    def new_simulation(self, simulation_name):
+        """Adds a new simulation to project.
 
         Args:
-            simulation_file: String representing file containing simulation data.
-            tab_id: Integer representing identifier for simulation's tab.
+            simulation_name: Name of the simulation
 
         Return:
             Returns new simulation or None if it wasn't added
         """
-        print(simulation_file)
-        simulation = None
-        simulation_filename = os.path.split(simulation_file)[1]
-        simulation_name = os.path.splitext(simulation_filename)
-        new_file = os.path.join(self.project.directory, simulation_filename)
+        simulation_directory = os.path.join(self._directory, simulation_name)
 
-        file_directory, file_name = os.path.split(simulation_file)
+        # Check if simulation already exists. In case of exception we should
+        # inform the user that another simulation name should be used.
+        if os.path.exists(simulation_directory): raise EEXIST; return
+
+        # Create a directory for new simulation.
         try:
-            if file_directory != self.project.directory and file_directory:
-                dirtyinteger = 2  # Begin from 2, since 0 and 1 would be confusing.
-                while os.path.exists(new_file):
-                    file_name = "{0}_{1}{2}".format(simulation_name[0],
-                                                    dirtyinteger,
-                                                    simulation_name[1])
-                    new_file = os.path.join(self.project.directory, file_name)
-                    dirtyinteger += 1
-                shutil.copyfile(simulation_file, new_file)
-                file_directory, file_name = os.path.split(new_file)
-
-                log = "Added new simulation {0} to the project.".format(
-                    file_name)
-                logging.getLogger("project").info(log)
-            keys = self.simulations.keys()
-            for key in keys:
-                if self.simulations[key].simulation_file == file_name:
-                    return simulation  # measurement = None
-            simulation = Simulation(new_file, self.project, tab_id)
-            # measurement.load_data()
-            self.simulations[tab_id] = simulation
+            os.makedirs(simulation_directory)
         except:
-            log = "Something went wrong while adding a new simulation."
-            logging.getLogger("project").critical(log)
-            print(sys.exc_info())  # TODO: Remove this.
-        return simulation
+            log_msg = "Failed creating directory for simulation" + simulation_name
+            logging.getLogger("project").critical(log_msg)
+            # TODO: Inform user also with a pop up window.
+            return
 
-    def remove_by_tab_id(self, tab_id):
-        """Removes simulation from simulations by tab id
+        # Add a new simulation by making a Simulation object and adding it to
+        # list of simulations.
+        try:
+            self.simulations.append(Simulation(simulation_directory))
+            log_msg =  "Added simulation " + simulation_name + " to the project."
+            logging.getLogger("project").info(log_msg)
+        except:
+            log_msg = "Something went wrong while adding simulation" + simulation_name
+            logging.getLogger("project").critical(log_msg)
+            # TODO: Inform user also with a pop up window.
 
-        Args:
-            tab_id: Integer representing tab identifier.
-        """
-
-        def remove_key(d, key):
-            r = dict(d)
-            del r[key]
-            return r
-
-        self.simulations = remove_key(self.simulations, tab_id)
-
+    # TODO: Function for removing simulation
 
 class Simulation:
     """Simulation class handles the simulation data."""
 
-    def __init__(self, command_file, project, tab_id):
-        """Inits Simulation.
+    def __init__(self, directory):
+        """Initializes the simulation.
 
         Args:
             project: Project class object.
         """
 
-        simulation_folder, simulation_name = os.path.split(command_file)
-        self.simulation_file = simulation_name  # With extension
-        self.simulation_name = os.path.splitext(simulation_name)[0]
-
-        self.project = project
-        self.directory = os.path.join(simulation_folder, self.simulation_name)
-
-        self.data = []
-        self.tab_id = tab_id
-
-        self.__make_directories(self.directory)
-        # self.set_loggers()
+        self._directory = directory
 
         # The settings that come from the project
-        self.__project_settings = self.project.settings
+        self._project_settings = self.project.settings
         # The settings that are individually set for this measurement
         self.simulation_settings = Settings(self.directory, self.__project_settings)
 
@@ -172,7 +145,7 @@ class Simulation:
             # logging.getLogger("project").info(log)
 
     def load_data(self):
-        """Loads measurement data from filepath
+        """Loads simulation data from filepath
         """
         # import cProfile, pstats
         # pr = cProfile.Profile()
@@ -281,17 +254,22 @@ class CallGetEspe(object):
         # TODO Read the parameters from the program
 
         # Example parameters:
-        input_file = "35Cl-85-LiMnO_Li.*.erd"
-        params = {"-beam 35Cl", "-energy 8.515", "-theta 41.12", "-tangle 20.6", "-timeres 250.0",
-                  "-toflen 0.623", "-solid 0.2", "-dose 8.1e12", "-avemass",
-                  "-density 4.98e16", "-dist recoiling.LiMnO_Li", "-ch 0.02"}  # recoiling file needs to be a parameter
-        params_string = " ".join(params)
+
+        params = " ".join(["-beam 35Cl", "-energy 8.515", "-theta 41.12",
+                           "-tangle 20.6", "-timeres 250.0", "-toflen 0.623",
+                           "-solid 0.2", "-dose 8.1e12", "-avemass",
+                           "-density 4.98e16", "-dist recoiling.LiMnO_Li",
+                           "-ch 0.02"])  # recoiling file needs to be a parameter
+        # params_string = " ".join(params)
         output_file = "LiMnO_Li.simu"
 
         # TODO: No cd-ing, do this with absolute paths
         self.command_win = "cd " + self.bin_dir + " && type " + input_file + \
                            " | " + os.getcwd() + "\external\Potku-bin\get_espe " + params_string + \
                            " > " + output_file
+        input_file = project.directory +"35Cl-85-LiMnO_Li.*.erd"
+        self.command_win = BIN_DIR + "get_espe " + params
+
         self.command_unix = "cd " + self.bin_dir + " && cat " + input_file + \
                             " | " + os.getcwd() + "/external/Potku-bin/get_espe " + params_string + \
                             " > " + output_file
