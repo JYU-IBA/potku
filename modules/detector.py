4a392242df8528a55cc93d2b7b02f5224eb68e5f
siansiir <siansiir@student.jyu.fi>
2018-05-03 13:10:59 +0300

    Detector to JSON and from JSON
    Detector converts to JSON and from JSON.

--- modules/detector.py
+++ modules/detector.py
@@ -12,6 +12,7 @@ import os
 import json
 import datetime
 import shutil
+import time
 
 from modules.foil import CircularFoil, RectangularFoil
 from modules.layer import Layer
@@ -20,68 +21,79 @@ from modules.element import Element
 
 
 class Detector:
-
     # request maybe only temporary parameter
-    __slots__ = "name", "description", "date", "type", "calibration", "foils",\
-                "tof_foils", "virtual_size", "tof_slope", "tof_offset",\
-                "angle_slope", "angle_offset", "path", "modification_time",\
-                "efficiencies", "efficiency_directory", "timeres"
+    __slots__ = "name", "description", "date", "detector_type", "calibration", "foils", \
+                "tof_foils", "virtual_size", "tof_slope", "tof_offset", \
+                "angle_slope", "angle_offset", "path", "modification_time", \
+                "efficiencies", "efficiency_directory", "timeres", \
+                "detector_fii", "detector_theta"
 
-    def __init__(self, name="Default", description="This a default detector.",
-                 modification_time=datetime.datetime.now(), type="TOF",
-                 calibration=CalibrationParameters(),
-                 foils=[CircularFoil("Default", 7.0, 256.0,
-                                     [Layer("First", [Element("C", 12.011, 1)],
-                                            0.1, 2.25)]),
-                        CircularFoil("Default", 9.0, 319.0,
-                                     [Layer("Second", [Element("C", 12.011, 1)],
-                                            13.3, 2.25)]),
-                        CircularFoil("Default", 18.0, 942.0,
-                                     [Layer("Third", [Element("C", 12.011, 1)],
-                                            44.4, 2.25)]),
-                        RectangularFoil("Default", (14.0, 14.0), 957.0,
-                                     [Layer("Fourth", [
-                                         Element("N", 14.00, 0.57),
-                                         Element("Si", 28.09, 0.43)],
-                                            1.0, 3.44)])],
-                 tof_foils=[1, 2], virtual_size=(2.0, 5.0), tof_slope=1e-11,
-                 tof_offset=1e-9, angle_slope=0, angle_offset=0, timeres=250.0):
+    def __init__(self, path, name="Default", description="This a default "
+                                                         "detector.",
+                 modification_time=time.time(), detector_type="TOF",
+                 calibration=CalibrationParameters(), foils=[CircularFoil(
+                "Default", 7.0, 256.0, [Layer("First", [Element("C", 12.011,
+                                                                1)], 0.1,
+                                              2.25)]), CircularFoil(
+                "Default", 9.0, 319.0, [Layer("Second", [Element("C", 12.011,
+                                                                 1)], 13.3,
+                                              2.25)]), CircularFoil(
+                "Default", 18.0, 942.0, [Layer("Third", [Element("C", 12.011,
+                                                                 1)], 44.4,
+                                               2.25)]), RectangularFoil(
+                "Default", 14.0, 14.0, 957.0, [Layer("Fourth", [Element(
+                    "N", 14.00, 0.57), Element("Si", 28.09, 0.43)], 1.0,
+                                                     3.44)])], tof_foils=[
+                1, 2], virtual_size=(2.0, 5.0), tof_slope=1e-11,
+                 tof_offset=1e-9, angle_slope=0, angle_offset=0,
+                 timeres=250.0, detector_fii=0, detector_theta=40):
         """Initialize a detector.
 
         Args:
             name: Detector name.
             description: Detector description.
-            modification_time: Time of modification of detector file.
-            type: Type of detector.
+            modification_time: Modification time of detector file in Unix time.
+            detector_type: Type of detector.
             calibration: Calibration parameters for detector.
             foils: Detector foils.
-            tof_foils: List of indexes that tell the index of tof foils in
-            foils list.
+            tof_foils: List of indexes of ToF foils in foils list.
             timeres: Time resolution.
 
         """
         # With this we get the path of the folder where the
         # .json file needs to go.
-        self.path = None
+        self.path = path
+
         self.name = name
         self.description = description
         self.modification_time = modification_time
-        self.type = type
+
+        self.detector_type = detector_type
         self.calibration = calibration
+        self.timeres = timeres
+        self.virtual_size = virtual_size
+        self.tof_slope = tof_slope
+        self.tof_offset = tof_offset
+        self.angle_slope = angle_slope
+        self.angle_offset = angle_offset
+        self.detector_fii = detector_fii
+        self.detector_theta = detector_theta
         self.foils = foils
         self.tof_foils = tof_foils
-        self.timeres = timeres
 
         self.efficiencies = []
         self.efficiency_directory = None
 
-    def create_folder_structure(self, path):
-        self.path = path
+        self.to_file(os.path.join(self.path))
+
+    def create_folder_structure(self, directory):
+        self.path = directory
 
         if not os.path.exists(self.path):
             os.makedirs(self.path)
 
-        self.efficiency_directory = os.path.join(self.path, "Efficiency_files")
+        self.efficiency_directory = os.path.join(self.path,
+                                                 "Efficiency_files")
         if not os.path.exists(self.efficiency_directory):
             os.makedirs(self.efficiency_directory)
 
@@ -129,8 +141,20 @@ class Detector:
         obj = json.load(open(file_path))
 
         # Below we do conversion from dictionary to Detector object
-        name = os.path.splitext(os.path.split(file_path)[1])[0]
-        angle = obj["angle"]
+        name = obj["name"]
+        description = obj["description"]
+        modification_time = obj["modification_time_unix"]
+        detector_type = obj["detector_type"]
+        calibration = None  # TODO
+        timeres = obj["timeres"]
+        virtual_size = obj["virtual_size"]
+        tof_slope = obj["tof_slope"]
+        tof_offset = obj["tof_offset"]
+        angle_slope = obj["angle_slope"]
+        angle_offset = obj["angle_offset"]
+        detector_fii = obj["detector_fii"]
+        detector_theta = obj["detector_theta"]
+        tof_foils = obj["tof_foils"]
         foils = []
 
         for foil in obj["foils"]:
@@ -140,18 +164,25 @@ class Detector:
 
             for layer in foil["layers"]:
                 layers.append(Layer(layer["name"],
-                                    Element.from_string(layer["elements"]),
+                                    layer["elements"],
                                     float(layer["thickness"]),
                                     float(layer["density"])))
 
             if foil["type"] == "circular":
                 foils.append(
-                    CircularFoil(foil["diameter"], distance, layers))
+                    CircularFoil(foil["name"], foil["diameter"], distance,
+                                 layers, foil["transmission"]))
             else:
                 foils.append(
-                    RectangularFoil(foil["size"], distance, layers))
+                    RectangularFoil(foil["name"], (foil["size"])[0],
+                                    (foil["size"])[1],
+                                    distance, layers, foil["transmission"]))
 
-        return cls(file_path, name, angle, foils) # TODO: Update this
+        return cls(file_path, name, description, modification_time,
+                   detector_type, calibration, foils, tof_foils,
+                   virtual_size, tof_slope,
+                   tof_offset, angle_slope, angle_offset,
+                   timeres, detector_fii, detector_theta)
 
     def to_file(self, file_path):
         """Save detector settings to a file.
@@ -162,11 +193,20 @@ class Detector:
         obj = {
             "name": self.name,
             "description": self.description,
-            "modification_time": str(self.modification_time),
-            "type": self.type,
+            "modification_time": str(datetime.datetime.fromtimestamp(
+                self.modification_time)),
+            "modification_time_unix": self.modification_time,
+            "detector_type": self.detector_type,
             "foils": [],
             "tof_foils": self.tof_foils,
-            "timeres": self.timeres
+            "timeres": self.timeres,
+            "virtual_size": self.virtual_size,
+            "tof_slope": self.tof_slope,
+            "tof_offset": self.tof_offset,
+            "angle_slope": self.angle_slope,
+            "angle_offset": self.angle_offset,
+            "detector_fii": self.detector_fii,
+            "detector_theta": self.detector_theta
         }
 
         for foil in self.foils:
@@ -196,7 +236,6 @@ class Detector:
 
         with open(file_path, "w") as file:
             json.dump(obj, file, indent=4)
-
 
 # class ToFDetector(Detector):
 #
