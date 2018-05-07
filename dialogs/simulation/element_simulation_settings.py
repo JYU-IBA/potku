8e9cc8ceebf7636ab2ced69a0b6f2026ce875a42
Samuel Martti Anton Kaiponen <samaanka@student.jyu.fi>
2018-05-07 16:13:58 +0300

    Changed the behaviour of ElementSettingsDialog regarding default settings.
    Now the user can toggle the default settings checkbox without losing the changes they've made. If default settings are used and applied, the specific .mcsimu setting file is removed if it exists.

--- dialogs/simulation/element_simulation_settings.py
+++ dialogs/simulation/element_simulation_settings.py
@@ -25,9 +25,10 @@ class ElementSimulationSettingsDialog(QtWidgets.QDialog):
         self.element_simulation = element_simulation
         self.temp_settings = {}
 
+        self.use_default_settings = True
+
         self.ui.useRequestSettingsValuesCheckBox.stateChanged.connect(
             self.toggle_settings)
-        self.import_default_settings()
 
         self.set_spinbox_maximums()
         self.import_specific_settings()
@@ -76,45 +77,16 @@ class ElementSimulationSettingsDialog(QtWidgets.QDialog):
         self.ui.minimumEnergyDoubleSpinBox.setValue(
             self.element_simulation.minimum_energy)
 
-    def import_default_settings(self):
-        default_element_simu = self.element_simulation.request\
-            .default_element_simulation
-
-        self.ui.typeOfSimulationComboBox.setCurrentIndex(
-            self.ui.typeOfSimulationComboBox.findText(
-                default_element_simu.simulation_type))
-        self.ui.modeComboBox.setCurrentIndex(
-            self.ui.modeComboBox.findText(
-                default_element_simu.simulation_mode))
-        self.ui.numberOfIonsSpinBox.setValue(
-            default_element_simu.number_of_ions)
-        self.ui.numberOfPreIonsSpinBox.setValue(
-            default_element_simu.number_of_preions)
-        self.ui.seedSpinBox.setValue(
-            default_element_simu.seed_number)
-        self.ui.numberOfRecoilsSpinBox.setValue(
-            default_element_simu.number_of_recoils)
-        self.ui.numberOfScalingIonsSpinBox.setValue(
-            default_element_simu.number_of_scaling_ions)
-        self.ui.minimumScatterAngleDoubleSpinBox.setValue(
-            default_element_simu.minimum_scattering_angle)
-        self.ui.minimumMainScatterAngleDoubleSpinBox.setValue(
-            default_element_simu.minimum_main_scattering_angle)
-        self.ui.minimumEnergyDoubleSpinBox.setValue(
-            default_element_simu.minimum_energy)
-
     def toggle_settings(self):
         """If request settings checkbox is checked, disables settings in dialog.
         Otherwise enables settings.
         """
         if self.ui.useRequestSettingsValuesCheckBox.isChecked():
-            self.ui.generalParametersGroupBox.setEnabled(False)
-            self.ui.physicalParametersGroupBox.setEnabled(False)
-            self.import_default_settings()
+            self.ui.settingsGroupBox.setEnabled(False)
+            self.use_default_settings = True
         else:
-            self.ui.generalParametersGroupBox.setEnabled(True)
-            self.ui.physicalParametersGroupBox.setEnabled(True)
-            self.import_specific_settings()
+            self.ui.settingsGroupBox.setEnabled(True)
+            self.use_default_settings = False
 
     def update_settings_and_close(self):
         """Updates settings and closes the dialog."""
@@ -122,35 +94,74 @@ class ElementSimulationSettingsDialog(QtWidgets.QDialog):
         self.close()
 
     def update_settings(self):
-        """Reads values from dialog and updates them in the element simulation.
+        """If default settings are used, put them to element simulation and
+        delete the specific setting file, if it exists. If default settings are
+        not used, read settings from dialog, put them to element simulation and
+        save them to file.
         """
-        self.element_simulation.name = \
-            self.ui.nameLineEdit.text()
-        self.element_simulation.description = \
-            self.ui.descriptionPlainTextEdit\
-            .toPlainText()
-        self.element_simulation.simulation_type = \
-            self.ui.typeOfSimulationComboBox.currentText()
-        self.element_simulation.simulation_mode = \
-            self.ui.modeComboBox.currentText()
-        self.element_simulation.number_of_ions = \
-            self.ui.numberOfIonsSpinBox.value()
-        self.element_simulation.number_of_preions = \
-            self.ui.numberOfPreIonsSpinBox\
-            .value()
-        self.element_simulation.seed_number = \
-            self.ui.seedSpinBox.value()
-        self.element_simulation.number_of_recoils = \
-            self.ui.numberOfRecoilsSpinBox.value()
-        self.element_simulation.number_of_scaling_ions = \
-            self.ui.numberOfScalingIonsSpinBox.value()
-        self.element_simulation.minimum_scattering_angle = \
-            self.ui.minimumScatterAngleDoubleSpinBox.value()
-        self.element_simulation.minimum_main_scattering_angle = \
-            self.ui.minimumMainScatterAngleDoubleSpinBox.value()
-        self.element_simulation.minimum_energy = \
-            self.ui.minimumEnergyDoubleSpinBox.value()
 
-        self.element_simulation.mcsimu_to_file(
-                os.path.join(self.element_simulation.directory,
-                             self.element_simulation.name + ".mcsimu"))
+        if self.use_default_settings:
+            default_element_simu = self.element_simulation.request \
+                .default_element_simulation
+
+            try:
+                os.remove(
+                    os.path.join(self.element_simulation.directory,
+                                 self.element_simulation.name + ".mcsimu"))
+            except OSError:
+                pass  # The file doesn't exist
+
+            self.element_simulation.name = \
+                default_element_simu.name
+            self.element_simulation.description = \
+                default_element_simu.description
+            self.element_simulation.simulation_type = \
+                default_element_simu.simulation_type
+            self.element_simulation.simulation_mode = \
+                default_element_simu.simulation_mode
+            self.element_simulation.number_of_ions = \
+                default_element_simu.number_of_ions
+            self.element_simulation.number_of_preions = \
+                default_element_simu.number_of_preions
+            self.element_simulation.seed_number = \
+                default_element_simu.seed_number
+            self.element_simulation.number_of_recoils = \
+                default_element_simu.number_of_recoils
+            self.element_simulation.number_of_scaling_ions = \
+                default_element_simu.number_of_scaling_ions
+            self.element_simulation.minimum_scattering_angle = \
+                default_element_simu.minimum_scattering_angle
+            self.element_simulation.minimum_main_scattering_angle = \
+                default_element_simu.minimum_main_scattering_angle
+            self.element_simulation.minimum_energy = \
+                default_element_simu.minimum_energy
+        else:
+            self.element_simulation.name = \
+                self.ui.nameLineEdit.text()
+            self.element_simulation.description = \
+                self.ui.descriptionPlainTextEdit.toPlainText()
+            self.element_simulation.simulation_type = \
+                self.ui.typeOfSimulationComboBox.currentText()
+            self.element_simulation.simulation_mode = \
+                self.ui.modeComboBox.currentText()
+            self.element_simulation.number_of_ions = \
+                self.ui.numberOfIonsSpinBox.value()
+            self.element_simulation.number_of_preions = \
+                self.ui.numberOfPreIonsSpinBox\
+                .value()
+            self.element_simulation.seed_number = \
+                self.ui.seedSpinBox.value()
+            self.element_simulation.number_of_recoils = \
+                self.ui.numberOfRecoilsSpinBox.value()
+            self.element_simulation.number_of_scaling_ions = \
+                self.ui.numberOfScalingIonsSpinBox.value()
+            self.element_simulation.minimum_scattering_angle = \
+                self.ui.minimumScatterAngleDoubleSpinBox.value()
+            self.element_simulation.minimum_main_scattering_angle = \
+                self.ui.minimumMainScatterAngleDoubleSpinBox.value()
+            self.element_simulation.minimum_energy = \
+                self.ui.minimumEnergyDoubleSpinBox.value()
+
+            self.element_simulation.mcsimu_to_file(
+                    os.path.join(self.element_simulation.directory,
+                                 self.element_simulation.name + ".mcsimu"))
