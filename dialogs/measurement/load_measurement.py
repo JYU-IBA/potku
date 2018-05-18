fec7934ce80b76df6bc93cd947b869a2da6fecba
Sinikka Siironen <sinikka.a.siironen@student.jyu.fi>
2018-05-18 09:28:49 +0300

    Checking for path, name and sample in new measurement
    When user adds a new measurement, path, name and sample are required.
    If they are missing, the fields are shown in red.

--- dialogs/measurement/load_measurement.py
+++ dialogs/measurement/load_measurement.py
@@ -8,7 +8,7 @@ Updated on 6.4.2018
 
 """
 from dialogs.new_sample import NewSampleDialog
-from modules.general_functions import open_file_dialog
+from modules.general_functions import open_file_dialog, check_text
 
 __author__ = "Severi Jääskeläinen \n Samuel Kaiponen \n Heta Rekilä \n Sinikka Siironen"
 __version__ = "2.0"
@@ -29,36 +29,66 @@ class LoadMeasurementDialog(QtWidgets.QDialog):
         """
         super().__init__()
 
-        self.ui = uic.loadUi(os.path.join("ui_files", "ui_new_measurement.ui"), self)
+        self.ui = uic.loadUi(os.path.join(
+            "ui_files", "ui_new_measurement.ui"), self)
 
         self.ui.browseButton.clicked.connect(self.__browse_files)
         self.ui.addSampleButton.clicked.connect(self.__add_sample)
         self.ui.loadButton.clicked.connect(self.__load_measurement)
         self.ui.cancelButton.clicked.connect(self.close)
-        self.name = None
+        self.name = ""
         self.sample = None
-        self.samples = samples
         self.directory = directory
 
         for sample in samples:
-            self.ui.samplesComboBox.addItem("Sample " + "%02d" % sample.serial_number + " " + sample.name)
-        
+            self.ui.samplesComboBox.addItem(
+                "Sample " + "%02d" % sample.serial_number + " " + sample.name)
+
+        if not samples:
+            self.ui.samplesComboBox.setStyleSheet(
+                'QComboBox { background-color: %s }' % "#f6989d")
+
+        self.ui.nameLineEdit.setStyleSheet(
+            'QLineEdit { background-color: %s }' % "#f6989d")
+        self.ui.nameLineEdit.textChanged.connect(lambda: self.__check_text(
+            self.ui.nameLineEdit))
+        self.ui.pathLineEdit.setStyleSheet(
+            'QLineEdit { background-color: %s }' % "#f6989d")
+        self.ui.pathLineEdit.textChanged.connect(lambda: self.__check_text(
+            self.ui.pathLineEdit))
+
         self.exec_()
 
     def __add_sample(self):
-        dialog = NewSampleDialog(self.samples)
+        dialog = NewSampleDialog()
         if dialog.name:
             self.ui.samplesComboBox.addItem(dialog.name)
-            self.ui.samplesComboBox.setCurrentIndex(self.ui.samplesComboBox.findText(dialog.name))
+            self.ui.samplesComboBox.setCurrentIndex(
+                self.ui.samplesComboBox.findText(dialog.name))
+            self.ui.samplesComboBox.setStyleSheet(
+                'QComboBox { background-color: %s }' % "#ffffff")
 
     def __load_measurement(self):
-        self.path = self.ui.pathLineEdit.text
+        self.path = self.ui.pathLineEdit.text()
         self.name = self.ui.nameLineEdit.text().replace(" ", "_")
         self.sample = self.ui.samplesComboBox.currentText()
         if not self.path:
+            self.ui.browseButton.setFocus()
+            return
+        if not self.name:
+            self.ui.nameLineEdit.setFocus()
+            return
+        if not self.sample:
+            self.ui.addSampleButton.setFocus()
             return
         self.close()
 
     def __browse_files(self):
-        self.filename = open_file_dialog(self, self.directory, "Select a measurement to load", "Raw Measurement (*.asc)")
+        self.filename = open_file_dialog(self, self.directory,
+                                         "Select a measurement to load",
+                                         "Raw Measurement (*.asc)")
         self.ui.pathLineEdit.setText(self.filename)
+
+    @staticmethod
+    def __check_text(input_field):
+        check_text(input_field)
