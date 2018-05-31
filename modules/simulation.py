5b414c01937fe6da0740a037e96120c482ce5137
Heta Liisa Rekilä <heta.l.rekila@student.jyu.fi>
2018-05-31 22:44:02 +0300

    Some minor changes that were somwhow left out from last commit.

--- modules/simulation.py
+++ modules/simulation.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 """
 Created on 26.2.2018
-Updated on 30.5.2018
+Updated on 31.5.2018
 
 Potku is a graphical user interface for analyzation and
 visualization of measurement data collected from a ToF-ERD
