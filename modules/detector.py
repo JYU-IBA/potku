20e1f5bd9895b60c6ed6b5b367e3e2eb3c303976
Heta Rekilä <heta.l.rekila@student.jyu.fi>
2018-12-17 15:15:41 +0200

    Changed the detector angle default.

--- modules/detector.py
+++ modules/detector.py
@@ -55,7 +55,7 @@ class Detector:
                  detector_type="TOF",
                  foils=None, tof_foils=None, virtual_size=(2.0, 5.0),
                  tof_slope=5.8e-11, tof_offset=-1.0e-9, angle_slope=0,
-                 angle_offset=0, timeres=250.0, detector_theta=40,
+                 angle_offset=0, timeres=250.0, detector_theta=41,
                  save_in_creation=True):
         """Initialize a detector.
 
