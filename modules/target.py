23bf5d92401849a731b2885f8c1752dd5cd9ca0f
Jussi <16700760+jussiks@users.noreply.github.com>
2020-04-07 09:27:51 +0300

    Refactored Target deserialization

--- modules/target.py
+++ modules/target.py
@@ -29,11 +29,10 @@ __author__ = "Severi Jääskeläinen \n Samuel Kaiponen \n Heta Rekilä \n" \
 __version__ = "2.0"
 
 import os
-
-from modules.element import Element
 import json
 import time
 
+from modules.element import Element
 from modules.layer import Layer
 
 
@@ -69,16 +68,19 @@ class Target:
         self.modification_time = modification_time
         self.description = description
         self.target_type = target_type
-        self.image_size = image_size
+        self.image_size = tuple(image_size)
         self.image_file = image_file
+        self.target_theta = target_theta
+
         if scattering_element is None:
             self.scattering_element = Element.from_string("4He 3.0")
         else:
             self.scattering_element = scattering_element
-        self.target_theta = target_theta
+
         if layers is None:
-            layers = []
-        self.layers = layers
+            self.layers = []
+        else:
+            self.layers = layers
 
     @classmethod
     def from_file(cls, target_file_path, measurement_file_path, request):
@@ -96,45 +98,35 @@ class Target:
         """
 
         with open(target_file_path) as tgt_file:
-            obj = json.load(tgt_file)
+            target = json.load(tgt_file)
 
-        # Below we do conversion from dictionary to Target object
-        name = obj["name"]
-        description = obj["description"]
-        modification_time_unix = obj["modification_time_unix"]
-        target_type = obj["target_type"]
-        scattering_element = Element.from_string(obj["scattering_element"])
-        image_size = tuple(obj["image_size"])
-        image_file = obj["image_file"]
+        target["modification_time"] = target.pop("modification_time_unix")
+        target["scattering_element"] = Element.from_string(
+            target["scattering_element"])
+        target["image_size"] = target["image_size"]
+
         layers = []
-
-        for layer in obj["layers"]:
-            elements = []
-            elements_str = layer["elements"]
-            for element_str in elements_str:
-                elements.append(Element.from_string(element_str))
-            layers.append(Layer(layer["name"],
-                                elements,
-                                layer["thickness"],
-                                layer["density"],
-                                layer["start_depth"]))
+        for layer in target.pop("layers"):
+            elements = [
+                Element.from_string(e)
+                for e in layer.pop("elements")
+            ]
+            layers.append(Layer(**layer, elements=elements))
 
         try:
             with open(measurement_file_path) as mesu:
-                obj = json.load(mesu)
-            target_theta = obj["geometry"]["target_theta"]
+                measurement = json.load(mesu)
+            target_theta = measurement["geometry"]["target_theta"]
         # If keys do not exist or measurement_file_path is empty or file
         # doesn't exist:
         except (KeyError, IsADirectoryError, FileNotFoundError, TypeError):
             target_theta = request.default_target.target_theta
 
-        return cls(name=name, description=description,
-                   modification_time=modification_time_unix,
-                   target_type=target_type,
-                   image_size=image_size, image_file=image_file,
-                   scattering_element=scattering_element,
-                   target_theta=target_theta,
-                   layers=layers)
+        # Note: this way of using kwargs does make it harder to maintain
+        # forward compatibility as there may be a need to add more fields
+        # to the json file. This could be remedied by adding **kwargs to
+        # the __init__ method.
+        return cls(**target, target_theta=target_theta, layers=layers)
 
     def to_file(self, target_file_path, measurement_file_path):
         """
