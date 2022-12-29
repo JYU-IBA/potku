febee60e944302d491aad36b5f8d6478eacd9734
Timo Leppälä <100778971+tleppala@users.noreply.github.com>
2022-12-29 10:23:23 +0200

    Print elements without splits(TODO)

--- widgets/matplotlib/measurement/energy_spectrum.py
+++ widgets/matplotlib/measurement/energy_spectrum.py
@@ -1094,7 +1094,7 @@ class IgnoreMeasurementElements:
             else:
                 color = self.selection_colors[color_string]
 
-            if len(key) == 3:
+            if True: #len(key) == 3: # TODO: printing splits properly
                 label = r"$^{" + str(isotope) + "}$" + element + rbs_string
             else:
                 label = r"$^{" + str(isotope) + "}$" + element \
