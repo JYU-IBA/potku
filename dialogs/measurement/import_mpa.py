4784ea86d91da8f7f3bf69c8a4362297c4563e07
lkmie <80851908+lkmie@users.noreply.github.com>
2023-03-18 19:00:55 +0200

    Edited file preview section of import_mpa.py

--- dialogs/measurement/import_mpa.py
+++ dialogs/measurement/import_mpa.py
@@ -61,7 +61,8 @@ class ImportDialogMPA(QtWidgets.QDialog):
         self.parent = parent
         self.__global_settings = self.parent.settings
         self.imported = False
-        self.files_added = {}  # Dictionary of files to be imported.
+        self.files_added = {}
+        self.__files_preview = {}
 
         self.button_import.clicked.connect(self.__import_files)
         self.button_cancel.clicked.connect(self.close)
@@ -83,13 +84,121 @@ class ImportDialogMPA(QtWidgets.QDialog):
             "Select binary files to be imported",
             "MPA format (*.lst)")
         df.add_imported_files_to_tree(self, files)
+        self.__load_files()
         self.__check_if_import_allowed()
+
+    def __load_file_preview(self, file=None, preview_max_len=64,
+                            preview_data_max_len=4):
+        """Load beginning of the file to preview window.
+
+        Args:
+            file: A string representing full path to the file.
+            preview_max_len: Maximum amount of lines to show in preview.
+            preview_data_max_len: Maximum amount of data lines to show
+                in preview.
+        """
+        if not file:
+            self.textEdit.clear()
+            self.button_coinc.setEnabled(False)
+            return
+        self.button_coinc.setEnabled(True)
+
+        preview_file_lines = self.__files_preview[file]
+        preview_max_len = min(preview_max_len, len(preview_file_lines))
+        data_title_line_index = None
+
+        # Find data title line
+        for i in range(preview_max_len):
+            line = preview_file_lines[i]
+            # Data title line ends with "Timestamp"
+            if line.strip()[-9:] == "Timestamp":
+                data_title_line_index = i
+                break
+
+        if data_title_line_index is not None:
+            preview_max_len = \
+                min(preview_max_len,
+                    data_title_line_index + preview_data_max_len + 1)
+
+        preview_string = f"{file} + \n\n"
+        for i in range(preview_max_len):
+            line = preview_file_lines[i]
+            preview_string += f"{i + 1}: {line}\n"
+        preview_string += "...\n"
+        self.textEdit.setText(preview_string)
+
+    def __load_files(self):
+        """Loads X lines of the data for rough estimation of values.
+        """
+        skip_length = 10
+        self.adc_occurance = {}
+        for file in self.files_added:
+            with open(file) as in_file:
+                self.__files_preview[file] = []
+                reading_data = False
+                i = 1
+                for line in in_file:
+                    if i >= 1000:
+                        break
+                    if not line:
+                        continue
+                    i += 1
+                    # Get ADC values
+                    if reading_data:
+                        split = line.strip().split("\t")
+                        if not split[0] in self.adc_occurance:
+                            self.adc_occurance[split[0]] = 1
+                        else:
+                            self.adc_occurance[split[0]] += 1
+                    # Automatically set good skip amount.
+                    elif line.strip()[-9:] == "Timestamp":
+                        skip_length = i
+                        reading_data = True
+                    self.__files_preview[file].append(line.strip())
+        # Automatically set good values.
+        sort_adc_occ = OrderedDict(sorted(self.adc_occurance.items(),
+                                          reverse=True))
+        adc_keys = sorted(self.adc_occurance.keys())
+
+        self.spin_skiplines.setValue(skip_length)
+        self.spin_adctrigger.setMinimum(int(adc_keys[0]))
+        self.spin_adctrigger.setMaximum(int(adc_keys[-1]))
+        self.spin_adctrigger.setValue(int(tuple(sort_adc_occ.keys())[0]))
+        self.spin_adccount.setValue(int(adc_keys[-1]) + 1)
+        self.__create_timing_spinbox()
+        self.__update_timings()
+        self.__insert_import_timings()
 
     def __check_if_import_allowed(self):
         """Toggle state of import button depending on if it is allowed.
         """
         root = self.treeWidget.invisibleRootItem()
         self.button_import.setEnabled(root.childCount() > 0)
+
+    def __coinc_calc(self):
+        """Calculate coincidence for selected
+        """
+        item = self.treeWidget.currentItem()
+        input_file = Path(item.file)
+
+        timing = dict()
+        timing_first = "1"
+        for coinc_timing in self.__added_timings.values():
+            if coinc_timing.is_not_trigger:
+                timing[coinc_timing.adc] = (coinc_timing.low.value(),
+                                            coinc_timing.high.value())
+                timing_first = coinc_timing.adc
+        # timing_first = timing.keys().next()
+        timing_low = self.__added_timings[timing_first].low
+        timing_high = self.__added_timings[timing_first].high
+        ImportTimingGraphDialog(
+            self, input_file, None, (timing_low, timing_high),
+            icon_manager=self.__icon_manager,
+            skip_lines=self.spin_skiplines.value(),
+            trigger=self.spin_adctrigger.value(),
+            adc_count=self.spin_adccount.value(),
+            timing=timing,
+            coinc_count=self.global_settings.get_import_coinc_count())
 
     @staticmethod
     def __convert_file(input_file, output_file):
