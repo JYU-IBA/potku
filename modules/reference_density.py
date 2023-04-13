9d4c4e440a1b3a86b2fe02df909501bc0dbd92b2
samivout <124705587+samivout@users.noreply.github.com>
2023-04-13 19:08:04 +0300

    Update ´ReferenceDensity´ class
    Class updated to support management of a user-entered manual value and an automatically calculated dynamic value.

--- modules/reference_density.py
+++ modules/reference_density.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 """
 Created on 3.1.2022
-Updated on 24.3.2023
+Updated on 13.4.2023
 
 Potku is a graphical user interface for analyzation and
 visualization of measurement data collected from a ToF-ERD
@@ -26,56 +26,87 @@ __author__ = "Joonas Koponen \n Tuomas Pitkänen \n Sami Voutilainen"
 __version__ = "2.0"
 
 from typing import List
+from typing import Optional
 
 from modules.general_functions import convert_amu_to_kg
 
 
 class ReferenceDensity:
-    """Class handles the calculation of the mean number density of a target up
-    to the thickness limit.
+    """Class manages the reference density value of a target. Handles the
+    dynamic calculation of the mean number density of a target up to the
+    thickness limit.
     """
 
-    def __init__(self, layers: List = None):
+    def __init__(self, layers: List = None,
+                 manual_density: Optional[float] = None,
+                 use_user_value: Optional[bool] = None):
         """Initializes a ReferenceDensity object.
 
         Args:
             layers: a list of layer objects.
+            manual_density: the user entered reference density value.
+            use_user_value: boolean managing the usage of dynamic and manual
+                reference density values.
         """
         self.layers = layers
-        self.reference_density = 0.0
+        self.dynamic_density = 0.0
         self.thickness_limit = 10.0    # Limit up to which layers are included
-        self.total_thickness = 0.0
+        self._total_thickness = 0.0
+        if use_user_value is not None:
+            self.use_user_value = use_user_value
+        else:
+            self.use_user_value = False
+
+        if manual_density is not None:
+            self.manual_density = manual_density
+        else:
+            self.manual_density = 0.0
+
+        self.update_dynamic_value()
+
+    def get_value(self):
+        """Returns either dynamic or manual reference density value based
+        on use_user_value. This method should be used as a way to retrieve
+        the reference density value elsewhere.
+        """
+        if self.use_user_value is True:
+            return self.manual_density
+        else:
+            return self.dynamic_density
 
     def update_layers(self, layers):
-        """Updates the layers used in reference density calculation.
+        """Updates the layers used in dynamic reference density calculation.
 
         Args:
             layers: a list of layer objects.
         """
         self.layers = layers
-        self.update_reference_density()
+        self.update_dynamic_value()
 
-    def update_reference_density(self):
-        """Calculates a new reference_density value.
+    def update_dynamic_value(self):
+        """Calculates a new dynamic reference density value.
         """
-        self.reference_density = 0.0
+        self.dynamic_density = 0.0
         if not self.layers:
             return
 
-        self.total_thickness = 0.0
+        self._total_thickness = 0.0
         for layer in self.layers:
-            self.add_layer_density(layer)
-            if self.total_thickness >= self.thickness_limit:
+            self._add_layer_density(layer)
+            if self._total_thickness >= self.thickness_limit:
                 break
 
-        if self.total_thickness != 0.0:
-            self.reference_density /= self.total_thickness
+        if self._total_thickness != 0.0:
+            self.dynamic_density /= self._total_thickness
         else:
-            self.reference_density = 0.0
+            self.dynamic_density = 0.0
 
-    def add_layer_density(self, layer):
+    def _add_layer_density(self, layer):
         """Calculates and adds the number density of a single layer multiplied
         by its thickness.
+
+        Args:
+            layer: a single layer object.
         """
         mean_atomic_mass = 0.0
 
@@ -86,8 +117,8 @@ class ReferenceDensity:
         layer_number_density = (layer.density / mean_atomic_mass)
 
         effective_thickness = layer.thickness
-        if self.total_thickness + effective_thickness > self.thickness_limit:
-            effective_thickness = self.thickness_limit - self.total_thickness
+        if self._total_thickness + effective_thickness > self.thickness_limit:
+            effective_thickness = self.thickness_limit - self._total_thickness
 
-        self.reference_density += layer_number_density * effective_thickness
-        self.total_thickness += effective_thickness
+        self.dynamic_density += layer_number_density * effective_thickness
+        self._total_thickness += effective_thickness
