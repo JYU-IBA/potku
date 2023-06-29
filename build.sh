a0a376b0ac22dfe9f4c1b1e6670d00dda2b885da
samivout <Zami123_7@hotmail.com>
2023-06-29 15:43:39 +0300

    Add optional commandline argument for toolchain file.

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@
 CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 EXT_DIR=$CUR_DIR/external
 
+if [ -z "$1" ]; then
+    echo "Using default toolchain file."
+else
+    echo "Using toolchain file at: $1"
+fi
+
 cd external
 
 make clean
@@ -13,7 +19,11 @@ cd submodules/jibal/
 rm build/CMakeCache.txt && echo Removed JIBAL cache file
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_RPATH="$EXT_DIR/lib" ../
+if [ -z "$1" ]; then
+    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_RPATH="$EXT_DIR/lib" ../
+else
+    cmake -DCMAKE_TOOLCHAIN_FILE=$1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_RPATH="$EXT_DIR/lib" ../
+fi
 make
 make install
 
@@ -21,6 +31,10 @@ cd ../../mcerd
 rm build/CMakeCache.txt && echo Removed MCERD cache file
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$EXT_DIR" -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_RPATH="$EXT_DIR/lib" ../
+if [ -z "$1" ]; then
+    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_RPATH="$EXT_DIR/lib" ../
+else
+    cmake -DCMAKE_TOOLCHAIN_FILE=$1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_RPATH="$EXT_DIR/lib" ../
+fi
 make
 make install
