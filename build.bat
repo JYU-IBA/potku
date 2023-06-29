3e18ca523352f4d9b66db397db4675dcc2abba2e
samivout <Zami123_7@hotmail.com>
2023-06-29 12:54:36 +0300

    Add optional commandline argument for toolchain file.

--- build.bat
+++ build.bat
@@ -2,8 +2,13 @@
 set CUR_DIR=%cd%
 set EXT_DIR=%CUR_DIR%\external
 
-@REM TODO take cmake options (-G and -DCMAKE_TOOLCHAIN_FILE) as command line
-@REM parameters.
+@REM TODO take cmake option -G as command line parameter.
+
+if "%1"=="" (
+	echo Using default toolchain file
+) else (
+	echo Using toolchain file at: %1
+)
 
 cd external
 
@@ -11,18 +16,27 @@ make clean
 make
 
 cd submodules\jibal\
-
 del /q build\CMakeCache.txt
 mkdir build
 cd build
-cmake -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%EXT_DIR% .. || EXIT /b 1
+
+if "%1"=="" (
+	cmake -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%EXT_DIR% .. || EXIT /b 1
+) else (
+	cmake -A x64 -DCMAKE_TOOLCHAIN_FILE=%1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%EXT_DIR% .. || EXIT /b 1
+)
 msbuild INSTALL.vcxproj /property:Configuration=Release || EXIT /b 1
 
 cd ..\..\mcerd
 del /q build\CMakeCache.txt
 mkdir build
 cd build
-cmake -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%EXT_DIR% -DCMAKE_INSTALL_PREFIX=%EXT_DIR% .. || EXIT /b 1
+
+if "%1"=="" (
+	cmake -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%EXT_DIR% -DCMAKE_INSTALL_PREFIX=%EXT_DIR% .. || EXIT /b 1
+) else (
+	cmake -A x64 -DCMAKE_TOOLCHAIN_FILE=%1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%EXT_DIR% -DCMAKE_INSTALL_PREFIX=%EXT_DIR% .. || EXIT /b 1
+)
 msbuild INSTALL.vcxproj /property:Configuration=Release || EXIT /b 1
 
 cd %CUR_DIR%
