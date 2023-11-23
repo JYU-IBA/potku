3b9c4c6677b16ff20af2b5bfb72067de39571b75
samivout <Zami123_7@hotmail.com>
2023-11-23 13:42:17 +0200

    Update reference density and labels only if simulation is not None.

--- widgets/matplotlib/simulation/composition.py
+++ widgets/matplotlib/simulation/composition.py
@@ -158,9 +158,9 @@ class _CompositionWidget(MatplotlibWidget):
         # Update layer start depths
         self.update_start_depths()
 
-        self.simulation.target.reference_density.update_layers(self.layers)
-
-        self.update_target_info_labels()
+        if self.simulation is not None:
+            self.simulation.target.reference_density.update_layers(self.layers)
+            self.update_target_info_labels()
 
         # Update canvas
         self.__update_figure(zoom_to_bottom=True)
@@ -372,9 +372,9 @@ class _CompositionWidget(MatplotlibWidget):
             self.__selected_layer = dialog.layer
             self.__update_figure(zoom_to_bottom=True)
 
-        self.simulation.target.reference_density.update_layers(self.layers)
-
-        self.update_target_info_labels()
+        if self.simulation is not None:
+            self.simulation.target.reference_density.update_layers(self.layers)
+            self.update_target_info_labels()
 
         if type(self.parent) is widgets.simulation.target.TargetWidget:
             self.parent.recoilRadioButton.setEnabled(True)
