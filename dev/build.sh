b41e37b9191f804ad54502e1330ebb21f23ee994
Jaakko Julin <jaakko.julin@jyu.fi>
2024-04-09 16:02:33 +0300

    Cleanup of C codes.

--- dev/build.sh
+++ dev/build.sh
@@ -7,48 +7,40 @@ EXT_DIR="$ROOT_DIR/external"
 
 if [ -z "$1" ]; then
     echo "Using default toolchain file."
+    toolchainargument=""
 else
-    echo "Using toolchain file at: $1"
+    echo "Using toolchain file at: \"$1\""
+    toolchainargument="-DCMAKE_TOOLCHAIN_FILE=\"$1\""
 fi
 
-cd external
+cd "external/submodules"
 
-make clean
-make
-
-cd submodules/jibal/
-
-rm build/CMakeCache.txt && echo Removed JIBAL cache file
-mkdir -p build
-cd build
-if [ -z "$1" ]; then
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$EXT_DIR" -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_RPATH="$EXT_DIR/lib" ../
-else
-    cmake -DCMAKE_TOOLCHAIN_FILE=$1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$EXT_DIR" -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_RPATH="$EXT_DIR/lib" ../
-fi
-make
-make install
-
-cd ../../mcerd
-rm build/CMakeCache.txt && echo Removed MCERD cache file
-mkdir -p build
-cd build
-if [ -z "$1" ]; then
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$EXT_DIR" -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_RPATH="$EXT_DIR/lib" ../
-else
-    cmake -DCMAKE_TOOLCHAIN_FILE=$1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$EXT_DIR" -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_RPATH="$EXT_DIR/lib" ../
-fi
-make
-make install
+for submodule in jibal erd_depth mcerd coinc; do
+    cd "${submodule}"
+    rm build/CMakeCache.txt && echo Removed $submodule CMakeCache.txt
+    mkdir -p build
+    cd build
+    cmake $toolchainargument -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$EXT_DIR" -DCMAKE_INSTALL_PREFIX="$EXT_DIR" -DCMAKE_INSTALL_RPATH="$EXT_DIR/lib" ../
+    make && make install
+    cd ../..
+done
 
 os_name=$(uname -s)
 gsl_libdir="$(pkg-config --variable=libdir gsl)"
 potku_libdir="$EXT_DIR/lib"
 gsl_version="$(pkg-config --modversion gsl|sed 's/^\(.*\)\.\(.*\)\.\(.*\)/\1\2/')" #E.g. 27 for 2.7.1
+
+mkdir -p "${potku_libdir}"
+
 if [ "$os_name" = "Linux" ]; then
-    cp "${gsl_libdir}/libgsl.so.${gsl_version}" "${gsl_libdir}/libgslcblas.so.0" "${potku_libdir}"
+    gsl_files="${gsl_libdir}/libgsl.so.${gsl_version} ${gsl_libdir}/libgslcblas.so.0"
+    chmod u+rw ${potku_libdir}/*.so
 fi
 
 if [ "$os_name" = "Darwin" ]; then
-    cp "${gsl_libdir}/libgsl.${gsl_version}.dylib" "${gsl_libdir}/libgslcblas.0.dylib" "${potku_libdir}"
+    gsl_files="${gsl_libdir}/libgsl.${gsl_version}.dylib ${gsl_libdir}/libgslcblas.0.dylib"
+    chmod u+rw ${potku_libdir}/*.dylib
 fi
+
+
+cp $gsl_files "${potku_libdir}"
